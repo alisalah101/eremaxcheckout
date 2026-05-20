e83295eb677edbb9bd49f532a581736a99e238ac
Mohammad Elfar <mohammad.elfar@mobileartsme.com>
2026-05-20 13:11:51 +0300

    merchant pass

--- lib/merchant-credentials.ts
+++ lib/merchant-credentials.ts
@@ -1,8 +1,8 @@
 export const MERCHANT_KEY =
-  process.env.MOBIBOX_MERCHANT_KEY || "eb515e92-a819-11f0-95c8-ae0005bd273e";
+  process.env.NEXT_PUBLIC_MERCHANT_KEY || "eb515e92-a819-11f0-95c8-ae0005bd273e";
 
 export const MERCHANT_PASS =
-  process.env.MOBIBOX_MERCHANT_PASS || "9e7d01b8a2ce585c1108432aa102b489";
+  process.env.NEXT_PUBLIC_MERCHANT_PASS || "9e7d01b8a2ce585c1108432aa102b489";
 
 export function getMerchantCredentials(): {
   merchant_key: string;
